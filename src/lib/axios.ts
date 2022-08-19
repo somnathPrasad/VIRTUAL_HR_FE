379cce8c8bf3fdfce1aa0b369d811e3a85a75051
Somnath Prasad <somnathprasad559@gmail.com>
2022-08-19 18:39:49 +0530

    baseurl axios dynamic

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,11 +1,16 @@
 import axios from "axios";
 
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000/api"
+    : "https://virtual-hr-be.herokuapp.com/api";
+
 const Axios = axios.create({
-  baseURL: "https://virtual-hr-be.herokuapp.com/api",
+  baseURL: BASE_URL,
 });
 
 export const AxiosPrivate = axios.create({
-  baseURL: "https://virtual-hr-be.herokuapp.com/api",
+  baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
